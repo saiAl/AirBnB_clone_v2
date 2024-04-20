a2d326a4dbd94464697defdd1613822009ee063c
Sai <ojiip95@gmail.com>
2024-04-20 20:48:23 +0200

    0x02: create Base object, delete() method, column represent id, created_at and updated_at to mapped to a table

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,33 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(
+            String(60), nullable=False,
+            primary_key=True, unique=True
+            )
+    created_at = Column(
+            DateTime, default=datetime.utcnow(), nullable=False
+            )
+    updated_at = Column(
+            DateTime, default=datetime.utcnow(), nullable=False
+            )
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +46,12 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        from models import storage
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +61,5 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        del deictionary["_sa_instance_state"]
         return dictionary
