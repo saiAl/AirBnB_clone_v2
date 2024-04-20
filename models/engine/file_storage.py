0872c39a9ba5ee1f59f77bb7b932effa6a218a52
Sai <ojiip95@gmail.com>
2024-04-20 20:27:43 +0200

    0x02: fix missing column

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if (cls == None)
+        if (cls == None):
             return FileStorage.__objects
         else:
             new = {}
