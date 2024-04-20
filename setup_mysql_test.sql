d35f4b6ecd114f81def3dd14f02c56600c2ceca2
Sai <ojiip95@gmail.com>
2024-04-20 20:20:28 +0200

    0x02: task 3, 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+--  script that prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES on hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
+
