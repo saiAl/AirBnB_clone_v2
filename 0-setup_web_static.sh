9091c122cb34bf9fd6a67bae414a994f046f48c5
Sai <ojiip95@gmail.com>
2024-05-04 19:30:55 +0200

    fix the conf string

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,10 @@
 # script that sets up your web servers for the deployment of web_static
 
 sudo apt-get -y update
-sudo apt-get -y install nginx
+sudo apt-get install -y nginx
 
-make_dir() {
-        if [ ! -d "$1" ]; then
-                sudo mkdir -p "$1"
-        fi
-}
-
-make_dir /data/web_static/shared/
-make_dir /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 
 if [ -f /data/web_static/current ]; then
 
@@ -26,7 +20,7 @@ echo "<h1>simple content</h1>" | sudo tee /data/web_static/releases/test/index.h
 sudo chown -hR "$USER":"$USER" /data/
 
 string_to_replace="# pass PHP scripts to FastCGI server"
-conf="location \/hbnb_Static \{\n\t\talias \/data\/web_static\/current\/;\n\t\}\n\n\t# pass PHP scripts to FastCGI server"
+conf="location \/hbnb_static \{\n\t\talias \/data\/web_static\/current\/;\n\t\}\n\n\t# pass PHP scripts to FastCGI server"
 path="/etc/nginx/sites-enabled/default"
 
 sudo sed -i "s/$string_to_replace/$conf/" "$path"
