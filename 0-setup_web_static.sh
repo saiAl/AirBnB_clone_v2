b77110358ea437a2475c0a2007c1cf1e1cad3336
Sai <ojiip95@gmail.com>
2024-05-04 18:26:30 +0200

    fix the deployment script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,25 +10,21 @@ make_dir() {
         fi
 }
 
-make_dir /data/web_static/releases/
 make_dir /data/web_static/shared/
 make_dir /data/web_static/releases/test/
-
 sudo chown -R "$USER":"$USER" /data/
 
-sudo echo "simple content" > /data/web_static/releases/test/index.html
 
 sudo rm -rf /data/web_Static/current
-
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-string_to_replace="# pass PHP scripts to FastCGI server"
+echo "<h1>simple content</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
+
+string_to_replace="# pass PHP scripts to FastCGI server"
 conf="location \/hbnb_Static \{\n\t\talias \/data\/web_static\/current\/;\n\t\}\n\n\t# pass PHP scripts to FastCGI server"
 path="/etc/nginx/sites-enabled/default"
 
 sudo sed -i "s/$string_to_replace/$conf/" "$path"
 
 sudo service nginx restart
-
-exit(0)
